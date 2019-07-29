823a7a6473f386fa0c2256c06ad067636491c645
Golding <keenan.golding@accenture.com>
2019-07-29 15:20:20 +0100

    implemented mongoose for OO data access

--- server.js
+++ server.js
@@ -1,14 +1,35 @@
+/*
+    IMPORTS AND VARS
+*/
+
 const PORT = 4000;
 
 const express = require('express');
 const app = express();
 
+const mongoose = require('mongoose');
+
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
+
+/*
+    CONTENT
+*/
+
+// mongodb
+mongoose.connect('mongodb://127.0.0.1:27017/ccx', { useNewUrlParser: true });
+const connection = mongoose.connection;
+
+connection.once('open', function() {
+    console.log("MongoDB database connection established successfully");
+})
+
+// loading cors and bodyparser
 app.use(cors());
 app.use(bodyParser.json());
 
+// listening for server
 app.listen(PORT, function() {
     console.log("Server is running on Port: " + PORT);
 });
